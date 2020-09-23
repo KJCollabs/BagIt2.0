9be67df43d692d61d8134b99046850a87ff94976
Jordan <jordan.a.neill@gmail.com>
2020-09-22 20:45:02 -0500

    placing bootstrap back in to non converted component so we can test the blueprint ui while we edit.

--- client/src/components/itemBar.js
+++ client/src/components/itemBar.js
@@ -2,26 +2,26 @@ import React from "react";
 import "./style.css";
 import Row from "react-bootstrap/Row";
 import Form from "react-bootstrap/Form";
-import { Col } from "@blueprintjs/core";
+import { Col } from "react-bootstrap";
 
 function ItemBar(props) {
-	return (
-		<Form className="itemBar">
-			{["checkbox"].map((type) => (
-				<Row className="mb-3" key={`inline-${type}`}>
-					<Form.Check
-						className="checkBox"
-						inline
-						type={type}
-						key={`inline-${type}-1`}
-					/>
-					<Col>
-						<p className="indItem">{props.itemName}</p>
-					</Col>
-				</Row>
-			))}
-		</Form>
-	);
+  return (
+    <Form className="itemBar">
+      {["checkbox"].map((type) => (
+        <Row className="mb-3" key={`inline-${type}`}>
+          <Form.Check
+            className="checkBox"
+            inline
+            type={type}
+            key={`inline-${type}-1`}
+          />
+          <Col>
+            <p className="indItem">{props.itemName}</p>
+          </Col>
+        </Row>
+      ))}
+    </Form>
+  );
 }
 
 export default ItemBar;
--- client/src/components/nav-Bar.js
+++ client/src/components/nav-Bar.js
@@ -1,62 +1,62 @@
 import React from "react";
 import { NavLink as RouterNavLink } from "react-router-dom";
-import { Container, Nav, Navbar } from "@blueprintjs/core";
+import { Container, Nav, Navbar } from "react-bootstrap";
 
 import { useAuth0 } from "@auth0/auth0-react";
 import LogoutButton from "./logoutButton";
 import LoginButton from "./loginButton";
 
 const MainNav = () => (
-	<Navbar className="mr-auto">
-		<Nav.Link
-			as={RouterNavLink}
-			to="/"
-			exact
-			activeClassName="router-link-exact-active"
-		>
-			Home
-		</Nav.Link>
+  <Navbar className="mr-auto">
+    <Nav.Link
+      as={RouterNavLink}
+      to="/"
+      exact
+      activeClassName="router-link-exact-active"
+    >
+      Home
+    </Nav.Link>
 
-		<Nav.Link
-			as={RouterNavLink}
-			to="/profile"
-			exact
-			activeClassName="router-link-exact-active"
-		>
-			Profile
-		</Nav.Link>
+    <Nav.Link
+      as={RouterNavLink}
+      to="/profile"
+      exact
+      activeClassName="router-link-exact-active"
+    >
+      Profile
+    </Nav.Link>
 
-		<Nav.Link
-			as={RouterNavLink}
-			to="/currentlist"
-			exact
-			activeClassName="router-link-exact-active"
-		>
-			Currentlist
-		</Nav.Link>
-	</Navbar>
+    <Nav.Link
+      as={RouterNavLink}
+      to="/currentlist"
+      exact
+      activeClassName="router-link-exact-active"
+    >
+      Currentlist
+    </Nav.Link>
+  </Navbar>
 );
 
 const AuthNav = () => {
-	const { isAuthenticated } = useAuth0();
+  const { isAuthenticated } = useAuth0();
 
-	return (
-		<Navbar className="justify-content-end">
-			{isAuthenticated ? <LogoutButton /> : <LoginButton />}
-		</Navbar>
-	);
+  return (
+    <Navbar className="justify-content-end">
+      {isAuthenticated ? <LogoutButton /> : <LoginButton />}
+    </Navbar>
+  );
 };
 
 const NavBar = () => {
-	return (
-		<Navbar bg="light" expand="md">
-			<Container>
-				<Navbar.Brand as={RouterNavLink} className="logo" to="/" />
-				<MainNav />
-				<AuthNav />
-			</Container>
-		</Navbar>
-	);
+  return (
+    <Navbar bg="light" expand="md">
+      <Container>
+        <Navbar.Brand as={RouterNavLink} className="logo" to="/" />
+        <MainNav />
+        <AuthNav />
+      </Container>
+    </Navbar>
+  );
 };
 
 export default NavBar;
