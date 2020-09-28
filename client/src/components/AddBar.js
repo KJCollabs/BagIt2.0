9281d9d84dbd8ab870392db6b78fe17a8f3ff40a
Jordan <jordan.a.neill@gmail.com>
2020-09-28 18:42:27 -0500

    fixing spacing on the imports on AddBar.js file in client/src/components

--- client/src/components/AddBar.js
+++ client/src/components/AddBar.js
@@ -1,52 +1,51 @@
 import React, { useState } from "react";
 import { FormGroup, InputGroup, Button } from "@blueprintjs/core";
-
 import "./style.css";
 
 function AddBar(props) {
-	const [dropdownOpen, setOpen] = useState(false);
+  const [dropdownOpen, setOpen] = useState(false);
 
-	const toggle = () => setOpen(!dropdownOpen);
-	return (
-		<div>
-			<header>
-				<FormGroup
-					helperText="Helper text with details..."
-					label="Label A"
-					labelFor="text-input"
-					labelInfo="(required)"
-					value={props.item}
-					onChange={props.changeText}
-				>
-					<InputGroup id="text-input" placeholder="Placeholder text" />
-				</FormGroup>
-				<h6>Select Category </h6>
-				<select
-					name="dropDownValue"
-					id="dropDown"
-					value={props.dropDownValue}
-					onChange={props.changeValue}
-				>
-					{props.categories.length > 0
-						? props.categories.map((category) => {
-								return (
-									<option key={category.id}>
-										{category.id + ". " + category.category_name}
-									</option>
-								);
-						  })
-						: ""}
-				</select>
+  const toggle = () => setOpen(!dropdownOpen);
+  return (
+    <div>
+      <header>
+        <FormGroup
+          helperText="Helper text with details..."
+          label="Label A"
+          labelFor="text-input"
+          labelInfo="(required)"
+          value={props.item}
+          onChange={props.changeText}
+        >
+          <InputGroup id="text-input" placeholder="Placeholder text" />
+        </FormGroup>
+        <h6>Select Category </h6>
+        <select
+          name="dropDownValue"
+          id="dropDown"
+          value={props.dropDownValue}
+          onChange={props.changeValue}
+        >
+          {props.categories.length > 0
+            ? props.categories.map((category) => {
+                return (
+                  <option key={category.id}>
+                    {category.id + ". " + category.category_name}
+                  </option>
+                );
+              })
+            : ""}
+        </select>
 
-				<Button
-					class=".bp3-intent-success .bp3-minimal .bp3-outline bp3-button bp3-icon-add"
-					onClick={props.handleSave}
-				>
-					Add Item
-				</Button>
-			</header>
-		</div>
-	);
+        <Button
+          class=".bp3-intent-success .bp3-minimal .bp3-outline bp3-button bp3-icon-add"
+          onClick={props.handleSave}
+        >
+          Add Item
+        </Button>
+      </header>
+    </div>
+  );
 }
 
 export default AddBar;
