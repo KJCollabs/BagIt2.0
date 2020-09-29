9730a4eb180db5ecfc822f0797529ea5d814006d
Jordan <jordan.a.neill@gmail.com>
2020-09-29 17:20:24 -0500

    fixed errors in client/compenent/index.js

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,7 +1,4 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import "~normalize.css";
-import "~@blueprintjs/core/lib/css/blueprint.css";
-import "~@blueprintjs/icons/lib/css/blueprint-icons.css";
 import "./style.css";
 import Loading from "./loading";
 import NavBar from "./NavBar";
