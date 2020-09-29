d6b48ea79bd72157681827f1da724b2d320a380d
Jordan <jordan.a.neill@gmail.com>
2020-09-29 16:43:03 -0500

    changed NavBar in the client/components to match others

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,7 +1,10 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import "~normalize.css";
+import "~@blueprintjs/core/lib/css/blueprint.css";
+import "~@blueprintjs/icons/lib/css/blueprint-icons.css";
 import "./style.css";
 import Loading from "./loading";
-import NavBar from "./nav-Bar";
+import NavBar from "./NavBar";
 import Highlight from "./highlight";
 import Hero from "./hero";
 import PrivateRoute from "./private-route";
