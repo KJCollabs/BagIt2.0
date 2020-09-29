ff716dc3e48968938a9f84619e0e367d7c7413a0
Jordan <jordan.a.neill@gmail.com>
2020-09-28 20:06:55 -0500

    fixing client/component/AddBar style

--- client/src/components/AddBar.js
+++ client/src/components/AddBar.js
@@ -3,49 +3,49 @@ import "./style.css";
 import { FormGroup, InputGroup, Button } from "@blueprintjs/core";
 
 function AddBar(props) {
-	const [dropdownOpen, setOpen] = useState(false);
+  const [dropdownOpen, setOpen] = useState(false);
 
-	const toggle = () => setOpen(!dropdownOpen);
-	return (
-		<div>
-			<header>
-				<FormGroup
-					labelFor="text-input"
-					value={props.item}
-					onChange={props.changeText}
-					style={{ marginBottom: "0.5em" }}
-				>
-					<InputGroup id="text-input" placeholder="Grocery Item" />
-				</FormGroup>
-				<h6>Select Category </h6>
-				<select
-					name="dropDownValue"
-					id="dropDown"
-					value={props.dropDownValue}
-					onChange={props.changeValue}
-					style={{ marginBottom: "0.5em" }}
-				>
-					{props.categories.length > 0
-						? props.categories.map((category) => {
-								return (
-									<option key={category.id}>
-										{category.id + ". " + category.category_name}
-									</option>
-								);
-						  })
-						: ""}
-				</select>
+  const toggle = () => setOpen(!dropdownOpen);
+  return (
+    <div>
+      <header>
+        <FormGroup
+          labelFor="text-input"
+          value={props.item}
+          onChange={props.changeText}
+          style={{ marginBottom: "0.5em" }}
+        >
+          <InputGroup id="text-input" placeholder="Grocery Item" />
+        </FormGroup>
+        <h6>Select Category </h6>
+        <select
+          name="dropDownValue"
+          id="dropDown"
+          value={props.dropDownValue}
+          onChange={props.changeValue}
+          style={{ marginBottom: "0.5em" }}
+        >
+          {props.categories.length > 0
+            ? props.categories.map((category) => {
+                return (
+                  <option key={category.id}>
+                    {category.id + ". " + category.category_name}
+                  </option>
+                );
+              })
+            : ""}
+        </select>
 
-				<Button
-					className="bp3-intent-success bp3-minimal bp3-outline bp3-button bp3-icon-add"
-					onClick={props.handleSave}
-					style={{ marginBottom: "0.5em" }}
-				>
-					Add Item
-				</Button>
-			</header>
-		</div>
-	);
+        <Button
+          className="bp3-intent-success bp3-minimal bp3-outline bp3-button bp3-icon-add"
+          onClick={props.handleSave}
+          style={{ marginBottom: "0.5em" }}
+        >
+          Add Item
+        </Button>
+      </header>
+    </div>
+  );
 }
 
 export default AddBar;
