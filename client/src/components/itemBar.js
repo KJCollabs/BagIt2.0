bf2e5dc2813797ae0df3406903aba75fa6d1235c
Jordan <jordan.a.neill@gmail.com>
2020-10-01 19:12:17 -0500

    styled the list in client/src/component/itemBar.js

--- client/src/components/itemBar.js
+++ client/src/components/itemBar.js
@@ -4,7 +4,14 @@ import { FormGroup, Switch } from "@blueprintjs/core";
 
 function ItemBar(props) {
   return (
-    <FormGroup>
+    <FormGroup
+      style={{
+        size: "200%",
+        height: "100%",
+        fontFamily: "monospace",
+        textAlign: "left",
+      }}
+    >
       <Switch {...props.itemName} labelElement={props.itemName} />
     </FormGroup>
   );
