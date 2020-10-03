5a4ed6a08124d75a1e05e9493b0d1b6cc3fe204a
Kevin Holder <kholder777@yahoo.com>
2020-10-03 14:40:16 -0500

    Middle of modifying NavBar.js. Still need to change the imports, and also link the buttons to their proper paths.

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -5,58 +5,63 @@ import { Container, Nav, Navbar } from "react-bootstrap";
 import { useAuth0 } from "@auth0/auth0-react";
 import LogoutButton from "./logoutButton";
 import LoginButton from "./loginButton";
-
+// still need to modify a few more things. Here's the link to the site for Blueprint Nav: https://blueprintjs.com/docs/#core/components/navbar.fixed-to-viewport-top
 const MainNav = () => (
-  <Navbar className="mr-auto">
-    <Nav.Link
+	<Nav className="mr-auto">
+		<button class="bp3-button bp3-minimal bp3-icon-home">Home</button>
+		{/* <Nav.Link
       as={RouterNavLink}
       to="/"
       exact
       activeClassName="router-link-exact-active"
     >
       Home
-    </Nav.Link>
+    </Nav.Link> */}
 
-    <Nav.Link
+		<button class="bp3-button bp3-minimal bp3-icon-user">Profile</button>
+		{/* <Nav.Link
       as={RouterNavLink}
       to="/profile"
       exact
       activeClassName="router-link-exact-active"
     >
       Profile
-    </Nav.Link>
+    </Nav.Link> */}
 
-    <Nav.Link
+		<button class="bp3-button bp3-minimal bp3-icon-document">
+			CurrentList
+		</button>
+		{/* <Nav.Link
       as={RouterNavLink}
       to="/currentlist"
       exact
       activeClassName="router-link-exact-active"
     >
       Currentlist
-    </Nav.Link>
-  </Navbar>
+    </Nav.Link> */}
+	</Nav>
 );
 
 const AuthNav = () => {
-  const { isAuthenticated } = useAuth0();
+	const { isAuthenticated } = useAuth0();
 
-  return (
-    <Navbar className="justify-content-end">
-      {isAuthenticated ? <LogoutButton /> : <LoginButton />}
-    </Navbar>
-  );
+	return (
+		<Navbar className="justify-content-end">
+			{isAuthenticated ? <LogoutButton /> : <LoginButton />}
+		</Navbar>
+	);
 };
 
 const NavBar = () => {
-  return (
-    <Navbar bg="dark" expand="md">
-      <Container>
-        <Navbar.Brand as={RouterNavLink} className="logo" to="/" />
-        <MainNav />
-        <AuthNav />
-      </Container>
-    </Navbar>
-  );
+	return (
+		<Navbar bg="dark" expand="md">
+			<Container>
+				<Navbar.Brand as={RouterNavLink} className="logo" to="/" />
+				<MainNav />
+				<AuthNav />
+			</Container>
+		</Navbar>
+	);
 };
 
 export default NavBar;
