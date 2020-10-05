16528367b10c24931325088934479eadbc0e04f0
Jordan <jordan.a.neill@gmail.com>
2020-10-05 14:26:12 -0500

    moved the useEffect to currentlist.js in the views so that we can refresh the page with the item and not have to manually refresh. I also added a [array, setArray] const to make this possible

--- client/src/views/Currentlist.js
+++ client/src/views/Currentlist.js
@@ -6,58 +6,60 @@ import API from "../utils/API";
 import { useAuth0 } from "@auth0/auth0-react";
 
 function Currentlist() {
-	const { user } = useAuth0();
-	const array = [1];
-	const [categories, setCategories] = useState([]);
-	const [dropDownValue, setDropDownValue] = useState("");
-	const [item, setItem] = useState("");
-	useEffect(() => {
-		API.getAllCategories().then((results) => {
-			setCategories(results.data.categories);
-		});
-	}, []);
+  const { user } = useAuth0();
+  const [array, setArray] = useState([1]);
+  const [categories, setCategories] = useState([]);
+  const [dropDownValue, setDropDownValue] = useState("");
+  const [item, setItem] = useState("");
+  useEffect(() => {
+    API.getAllCategories().then((results) => {
+      setCategories(results.data.categories);
+    });
+  }, []);
+  useEffect(() => {
+    API.getAllItems()
+      .then((res) => setArray(res.data.items))
+      .catch((err) => console.log(err));
+  }, []);
+  const changeValue = (event) => {
+    const { name, value } = event.target;
+    setDropDownValue(value);
+  };
+  const changeText = (event) => {
+    const { name, value } = event.target;
+    setItem(value);
+  };
 
-	const changeValue = (event) => {
-		const { name, value } = event.target;
-		setDropDownValue(value);
-	};
-	const changeText = (event) => {
-		const { name, value } = event.target;
-		setItem(value);
-	};
-
-	const handleSave = (event) => {
-		const id = dropDownValue.split(" ")[0];
-		const newItem = {
-			categoryId: id,
-			item: item,
-			user: user.name,
-			email: user.email,
-		};
-		API.createItem(newItem).then((results) => {
-			// console.log(results);
-			//TODO update state to render new stuff
-			// window.location.reload();
-		});
-	};
-	return (
-		<div>
-			<AddBar
-				categories={categories}
-				changeValue={changeValue}
-				dropDownValue={dropDownValue}
-				item={item}
-				handleSave={handleSave}
-				changeText={changeText}
-			/>
-			<Auth0ProviderWithHistory>
-				{/* //map this department */}
-				{array.map((DeptArray) => {
-					return <Department key={DeptArray} />;
-				})}
-				{/* TODO Array of departments. Map departments too. Look into props.*/}
-			</Auth0ProviderWithHistory>
-		</div>
-	);
+  const handleSave = (event) => {
+    const id = dropDownValue.split(". ")[0];
+    const newItem = {
+      categoryId: id,
+      item: item,
+      user: user.name,
+      email: user.email,
+    };
+    API.createItem(newItem).then((results) => {
+      API.getAllItems().then((response) => {
+        console.log(response.data.items);
+        setArray(response.data.items);
+        setItem("");
+      });
+    });
+  };
+  return (
+    <div>
+      <AddBar
+        categories={categories}
+        changeValue={changeValue}
+        dropDownValue={dropDownValue}
+        item={item}
+        handleSave={handleSave}
+        changeText={changeText}
+      />
+      <Auth0ProviderWithHistory>
+        <Department array={array} />;
+      </Auth0ProviderWithHistory>
+    </div>
+  );
 }
 export default Currentlist;
