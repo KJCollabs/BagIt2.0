1190a76e39d939ad1d7f27c59c5c789d42030e31
Jordan <jordan.a.neill@gmail.com>
2020-10-05 14:23:14 -0500

    converted navbar to blueprint ui and changed the link to link to the correct pages

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,47 +1,38 @@
 import React from "react";
-import { NavLink as RouterNavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 import {
   Navbar,
+  Button,
   NavbarGroup,
   NavbarHeading,
   NavbarDivider,
 } from "@blueprintjs/core";
+// import {Container } from "react-bootstrap";
 import { useAuth0 } from "@auth0/auth0-react";
 import LogoutButton from "./logoutButton";
 import LoginButton from "./loginButton";
+import "./style.css";
 // still need to modify a few more things. Here's the link to the site for Blueprint Nav: https://blueprintjs.com/docs/#core/components/navbar.fixed-to-viewport-top
 const MainNav = () => (
   <Navbar>
     <NavbarGroup className="mr-auto">
       <NavbarHeading>
-        <Nav.Link
-          as={RouterNavLink}
-          to="/"
-          exact
-          activeClassName="router-link-exact-active"
-        >
-          <button class="bp3-button bp3-minimal bp3-icon-home">Home</button>
-        </Nav.Link>
+        <Link to="/" exact activeClassName="router-link-exact-active">
+          <Button className="bp3-button bp3-minimal bp3-icon-home">Home</Button>
+        </Link>
       </NavbarHeading>
       <NavbarDivider />
-      <Nav.Link
-        as={RouterNavLink}
-        to="/profile"
-        exact
-        activeClassName="router-link-exact-active"
-      >
-        <button class="bp3-button bp3-minimal bp3-icon-user">Profile</button>
-      </Nav.Link>
-      <Nav.Link
-        as={RouterNavLink}
-        to="/currentlist"
-        exact
-        activeClassName="router-link-exact-active"
-      >
-        <button class="bp3-button bp3-minimal bp3-icon-document">
+      <Link to="/profile" exact activeClassName="router-link-exact-active">
+        <Button className="bp3-button bp3-minimal bp3-icon-user">
+          Profile
+        </Button>
+      </Link>
+      <Link to="/currentlist" exact activeClassName="router-link-exact-active">
+        <Button className="bp3-button bp3-minimal bp3-icon-document">
           CurrentList
-        </button>
-      </Nav.Link>
+        </Button>
+      </Link>
+      <AuthNav />
     </NavbarGroup>
   </Navbar>
 );
@@ -49,8 +40,12 @@ const AuthNav = () => {
   const { isAuthenticated } = useAuth0();
 
   return (
-    <Navbar className="justify-content-end">
-      {isAuthenticated ? <LogoutButton /> : <LoginButton />}
+    <Navbar className="logButton">
+      {isAuthenticated ? (
+        <LogoutButton className="logout" />
+      ) : (
+        <LoginButton className="login" />
+      )}
     </Navbar>
   );
 };
@@ -58,11 +53,10 @@ const AuthNav = () => {
 const NavBar = () => {
   return (
     <Navbar bg="dark" expand="md">
-      <Container>
-        <Navbar.Brand as={RouterNavLink} className="logo" to="/" />
-        <MainNav />
-        <AuthNav />
-      </Container>
+      {/* <Container> */}
+      {/* <Navbar.Brand className="logo" to="/" /> */}
+      <MainNav />
+      {/* </Container> */}
     </Navbar>
   );
 };
