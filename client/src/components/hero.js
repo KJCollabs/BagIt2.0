f9806e88d1ef847571f0a50b2c14d31ab3e5c0c1
Jordan <jordan.a.neill@gmail.com>
2020-10-05 14:54:04 -0500

    added text under the title to explain the site

--- client/src/components/hero.js
+++ client/src/components/hero.js
@@ -5,6 +5,7 @@ const Hero = () => (
   <div className="text-center hero my-5">
     <img className="mb-3 app-logo" src={logo} alt="BagIt logo" width="120" />
     <h1>Welcome to BagIt 2.0 </h1>
+    <h5>Login to start your organized shopping list!</h5>
   </div>
 );
 
