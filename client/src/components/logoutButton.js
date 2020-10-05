e16175813caa958d84c91a0338049ac7acc43a56
Jordan <jordan.a.neill@gmail.com>
2020-10-05 14:21:50 -0500

    imported ./style.css and changed the color based on the blueprint ui css

--- client/src/components/logoutButton.js
+++ client/src/components/logoutButton.js
@@ -1,22 +1,22 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 import { Button } from "@blueprintjs/core";
-
+import "./style.css";
 const LogoutButton = () => {
-	const { logout } = useAuth0();
-	return (
-		<Button
-			onClick={() =>
-				logout({
-					returnTo: window.location.origin,
-				})
-			}
-			variant="danger"
-			className="btn-margin"
-		>
-			Log Out
-		</Button>
-	);
+  const { logout } = useAuth0();
+  return (
+    <Button
+      onClick={() =>
+        logout({
+          returnTo: window.location.origin,
+        })
+      }
+      intent="danger"
+      className="logout"
+    >
+      Log Out
+    </Button>
+  );
 };
 
 export default LogoutButton;
