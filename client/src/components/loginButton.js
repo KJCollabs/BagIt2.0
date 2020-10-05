1d5145b29c8aee4f8e7dc3c8dbebedf95d72e971
Jordan <jordan.a.neill@gmail.com>
2020-10-05 14:21:40 -0500

    imported ./style.css and changed the color based on the blueprint ui css

--- client/src/components/loginButton.js
+++ client/src/components/loginButton.js
@@ -1,18 +1,18 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 import { Button } from "@blueprintjs/core";
-
+import "./style.css";
 const LoginButton = () => {
-	const { loginWithRedirect } = useAuth0();
-	return (
-		<Button
-			onClick={() => loginWithRedirect()}
-			variant="primary"
-			className="btn-margin"
-		>
-			Log In
-		</Button>
-	);
+  const { loginWithRedirect } = useAuth0();
+  return (
+    <Button
+      onClick={() => loginWithRedirect()}
+      intent="success"
+      className="login"
+    >
+      Log In
+    </Button>
+  );
 };
 
 export default LoginButton;
