dc9c39e1c31cd7c2a936678143e4323be6155d16
Jordan <jordan.a.neill@gmail.com>
2020-10-05 14:19:00 -0500

    added a value ro the InputGroup in the client/src/component/AddBar.js so that we can clear the add bar after something is added to the bar. also added the category id to correctly post the id in the database

--- client/src/components/AddBar.js
+++ client/src/components/AddBar.js
@@ -3,54 +3,55 @@ import { FormGroup, InputGroup, Button } from "@blueprintjs/core";
 import "./style.css";
 
 function AddBar(props) {
-	const [dropdownOpen, setOpen] = useState(false);
+  const [dropdownOpen, setOpen] = useState(false);
 
-	const toggle = () => setOpen(!dropdownOpen);
-	return (
-		<div>
-			<header>
-				<FormGroup
-					labelInfo="(required)"
-					labelFor="text-input"
-					value={props.item}
-					style={{ marginBottom: "0.5em" }}
-				>
-					<InputGroup
-						id="text-input"
-						placeholder="Grocery Item"
-						onChange={props.changeText}
-					/>
-				</FormGroup>
-				<h6>Select Category </h6>
-				<select
-					name="dropDownValue"
-					id="dropDown"
-					value={props.dropDownValue}
-					onChange={props.changeValue}
-					style={{ marginBottom: "0.5em" }}
-				>
-					{props.categories.length > 0
-						? props.categories.map((category) => {
-								return (
-									<option key={category.id}>
-										{/*Removed the following from inside the {} from the next line down: category.id + ". " +  */}
-										{category.category_name}
-									</option>
-								);
-						  })
-						: ""}
-				</select>
+  const toggle = () => setOpen(!dropdownOpen);
+  return (
+    <div>
+      <header>
+        <FormGroup
+          labelInfo="(required)"
+          labelFor="text-input"
+          value={props.item}
+          style={{ marginBottom: "0.5em" }}
+        >
+          <InputGroup
+            id="text-input"
+            placeholder="Grocery Item"
+            onChange={props.changeText}
+            value={props.item}
+          />
+        </FormGroup>
+        <h6>Select Category </h6>
+        <select
+          name="dropDownValue"
+          id="dropDown"
+          value={props.dropDownValue}
+          onChange={props.changeValue}
+          style={{ marginBottom: "0.5em" }}
+        >
+          {console.log(props.categories, "categories")}
+          {props.categories.length > 0
+            ? props.categories.map((category) => {
+                return (
+                  <option key={category.id}>
+                    {category.id + ". " + category.category_name}
+                  </option>
+                );
+              })
+            : ""}
+        </select>
 
-				<Button
-					className="bp3-intent-success bp3-minimal bp3-outline bp3-button bp3-icon-add"
-					onClick={props.handleSave}
-					style={{ marginBottom: "0.5em" }}
-				>
-					Add Item
-				</Button>
-			</header>
-		</div>
-	);
+        <Button
+          className="bp3-intent-success bp3-minimal bp3-outline bp3-button bp3-icon-add"
+          onClick={props.handleSave}
+          style={{ marginBottom: "0.5em" }}
+        >
+          Add Item
+        </Button>
+      </header>
+    </div>
+  );
 }
 
 export default AddBar;
